2314fc8ef5eee6ce2b0585228a29222475a4fee3
PALAK <8.palak.8@gmail.com>
2023-06-29 11:19:28 +0530

    first commit

---
+++ BlankProcess/information.py
@@ -0,0 +1,42 @@
+from PIL import Image, ImageTk
+import tkinter as tk
+
+# Function to close the program
+def close_program():
+    root.destroy()
+
+# Load the image
+image_path = r"D:\documents\Documents\UiPath\BlankProcess\INFORMATION.png"  # Replace with the actual path to your image file
+image = Image.open(image_path)
+
+# Resize the image to a desired width and height
+new_width = 800  # Adjust as needed
+new_height = 600  # Adjust as needed
+image = image.resize((new_width, new_height))
+
+# Create a Tkinter window
+root = tk.Tk()
+
+# Set the window title
+root.title("Process Completed")
+
+# Set the background color of the window
+root.configure(bg="black")
+
+# Convert the image to Tkinter-compatible format
+tk_image = ImageTk.PhotoImage(image)
+
+# Create a label and display the image
+label = tk.Label(root, image=tk_image)
+label.pack()
+
+# Create a frame to hold the button
+frame = tk.Frame(root, bg="orange")
+frame.pack()
+
+# Create the "Next" button
+next_button = tk.Button(frame, text="  Next  ", command=close_program, bg="orange", fg="black")
+next_button.pack(side=tk.RIGHT)
+
+# Run the Tkinter event loop
+root.mainloop()
--- BlankProcess1/test.py
+++ BlankProcess1/test.py
@@ -7,28 +7,28 @@ def get_user_input():
     if name:
         message_label.config(text=f'Hello, {name}!')
     else:
-        message_label.config(text="Please enter your name.")
+        message_label.config(text="Enter the name of drive folder to be created for assignment submission:")
     root.destroy()  # Close the window
 
 def hello():
     return name
 
 root = tk.Tk()
-root.title("Custom Dialog Box")
+root.title("Assignment folder name")
 
 # Customize the window's appearance and size
-root.geometry("400x300")
-root.configure(bg="white")
+root.geometry("1100x800")
+root.configure(bg="black")
 
 # Load and display the background image
-image_path = r"C:\Users\hp\Downloads\_ecefca38-64f6-4d93-8294-e5cdae3d1e8b.jpeg"
+image_path = r"D:\documents\Documents\UiPath\BlankProcess\QUESTION.png"
 image = Image.open(image_path)
 background_image = ImageTk.PhotoImage(image)
 background_label = tk.Label(root, image=background_image)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 # Add a label to display the message
-message_label = tk.Label(root, text="What is your name?", font=("Arial", 16), bg="white")
+message_label = tk.Label(root, text="Enter the name of drive folder\n[to be created for assignment submission]", font=("Arial", 16), bg="blue")
 message_label.pack(pady=50)
 
 # Add an entry for user input
@@ -37,7 +37,7 @@ name_entry.pack(pady=10)
 
 # Add a button to submit the input
 submit_button = tk.Button(root, text="Submit", font=("Arial", 12), command=get_user_input)
-submit_button.pack(pady=10)
+submit_button.pack(pady=40)
 
 # Run the event loop to display the window
 root.mainloop()
